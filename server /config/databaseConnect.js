869f4dd08cba71bcfb0b15102efadb5dbbb24017
Pranesh Deshmukh <praneshd2003@gmail.com>
2023-08-21 23:38:45 +0530

    database connection

---
+++ server /config/databaseConnect.js
@@ -0,0 +1,20 @@
+
+import mongoose from "mongoose";
+mongoose.set('strictQuery',false);
+
+const connectionToDb = async () => {
+    try {
+        const {connection} = await mongoose.connect(
+            // process.env.MONGO_URL || 
+            'mongodb://127.0.0.1:27017/lms'
+        );
+            if (connection) {
+                console.log(`CONNECTED TO MongoDB: ${connection.host}`);
+            }
+        } catch (e) {
+            console.log(e);
+            process.exit(1);
+    }
+}
+
+export default connectionToDb;
